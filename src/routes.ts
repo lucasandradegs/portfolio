5859bf0fc5d6e32d9d4b86fd3d635aba40426170
Lucas Andrade <contato.developerlucas@gmail.com>
2022-12-20 23:24:19 -0300

    Middleware autorização

--- src/routes.ts
+++ src/routes.ts
@@ -3,19 +3,20 @@ import { authController } from './controllers/authController'
 import { categoriesController } from './controllers/categoriesController'
 import { projectsController } from './controllers/projectsController'
 import { videosController } from './controllers/videosController'
+import { ensureAuth } from './middlewares/auth'
 
 const router = express.Router()
 
 router.post('/auth/register', authController.register)
 router.post('/auth/login', authController.login)
 
-router.get('/categories', categoriesController.index)
-router.get('/categories/:id', categoriesController.show)
+router.get('/categories', ensureAuth, categoriesController.index)
+router.get('/categories/:id',ensureAuth, categoriesController.show)
 
-router.get('/projects/featured', projectsController.featured)
+router.get('/projects/featured', ensureAuth, projectsController.featured)
 router.get('/projects/newest', projectsController.newest)
-router.get('/projects/search', projectsController.search)
-router.get('/projects/:id', projectsController.show)
+router.get('/projects/search', ensureAuth, projectsController.search)
+router.get('/projects/:id', ensureAuth, projectsController.show)
 
 router.get('/videos/stream', videosController.stream)
 export { router }
